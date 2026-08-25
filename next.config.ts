3dcac8d465f11a05a5ff00aeaad2478c8ba7d408
nothenrynguyen <nothenrynguyen@gmail.com>
2026-08-25 03:43:13 -0700

    Add company logos to job cards

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    remotePatterns: [new URL("https://img.logo.dev/**")],
+  },
 };
 
 export default nextConfig;
